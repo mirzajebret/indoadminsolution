83d8863160c8036a0633da0a05a8353c4462b2ab
Mirza Alby Assidiqie <94514652+mirzajebret@users.noreply.github.com>
2024-12-20 15:55:41 +0700

    Update hero-carousel.js

--- hero-carousel.js
+++ hero-carousel.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
     showSlide(currentSlide);
 
     // Ganti slide setiap 5 detik
-    setInterval(nextSlide, 30000);
+    setInterval(nextSlide, 3000);
 
     // Variabel untuk menyimpan posisi sentuhan
     let startX = 0;
