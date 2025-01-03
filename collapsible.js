0c9d9f330d850d65e03ff710d989ead7802de7a9
Mirza Alby Assidiqie <94514652+mirzajebret@users.noreply.github.com>
2025-01-03 08:16:52 +0700

    Update collapsible.js

--- collapsible.js
+++ collapsible.js
@@ -1,14 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     var coll = document.getElementsByClassName("collapsible");
-    for (var i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block";
+
+    var applyCollapsible = function() {
+        if (window.innerWidth <= 768) {
+            for (var i = 0; i < coll.length; i++) {
+                coll[i].addEventListener("click", toggleCollapsible);
             }
-        });
-    }
+        } else {
+            for (var i = 0; i < coll.length; i++) {
+                coll[i].removeEventListener("click", toggleCollapsible);
+                coll[i].classList.remove("active");
+                coll[i].nextElementSibling.style.display = "block";
+            }
+        }
+    };
+
+    var toggleCollapsible = function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    };
+
+    window.addEventListener("resize", applyCollapsible);
+    applyCollapsible();
 });
